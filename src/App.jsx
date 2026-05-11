104e263cdc1fb974ec72f82a9bc8ab619fb588ee
kunj-pandya <kunjpandya998@gmail.com>
2026-05-11 13:05:22 +0530

    init setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Design Delivery</h1>
+      <Button>Button</Button>
+      <h2>testing</h2>
+    </>
+  )
+}
+
+export default App
